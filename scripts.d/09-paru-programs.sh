87034136b357ddde7980893b33cd19ecb5801f44
Cristian Chiru <cristi.chiru@gmail.com>
2024-06-26 02:45:20 +0300

    add rustdesk config

--- scripts.d/09-paru-programs.sh
+++ scripts.d/09-paru-programs.sh
@@ -48,6 +48,21 @@ configure_gnome-ssh-askpass4-git() {
   fi
 }
 
+configure_rustdesk-bin() {
+  _BIN_NAME=rustdesk
+  if [[ -d "${BASH_SOURCE%/*}/.config/$_BIN_NAME" ]]; then
+  ## Local users with sudo rights
+  while IFS= read -r line; do
+    user=$(cut -d: -f1 <<< "$line")
+    ## is a sudoer?
+    sudo -lU "$user" | grep -q 'not allowed' && continue
+    homedir=$(cut -d: -f6 <<< "$line")
+    sudo cp -uva "${BASH_SOURCE%/*}/.config/$_BIN_NAME" "$homedir/.config/" || continue
+    sudo chown -R "$user" "$homedir/.config/$_BIN_NAME"
+  done < <(getent passwd | grep -v nologin)
+fi
+}
+
 ## Deps
 which paru &>/dev/null || "${BASH_SOURCE%/*}/08-paru.sh"
 
