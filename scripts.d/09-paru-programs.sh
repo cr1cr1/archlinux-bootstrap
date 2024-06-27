c29809c75b688532832e408f3678b45a53025cc8
Cristian Chiru <cristi.chiru@gmail.com>
2024-06-27 21:05:59 +0300

    paru-programs deps

--- scripts.d/09-paru-programs.sh
+++ scripts.d/09-paru-programs.sh
@@ -72,6 +72,11 @@ configure_sunshine-bin() {
 
 ## Deps
 which paru &>/dev/null || "${BASH_SOURCE%/*}/00-paru.sh"
+INSTALLER='sudo pacman -Sy --noconfirm --needed'
+for c in debugedit fakeroot; do
+  which $c &>/dev/null || $INSTALLER $c
+done
+sudo pacman -Q binutils &>/dev/null || $INSTALLER binutils
 
 for k in 8DFE60B7327D52D6 93BDB53CD4EBC740; do
   gpg --list-keys "$k" &>/dev/null || gpg --keyserver hkps://pgp.surf.nl --recv-key "$k"
