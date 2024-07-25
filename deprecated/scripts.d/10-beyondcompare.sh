0f758934d704a77618e0b2192c9dbc999b98cb12
Cristian Chiru <cristi.chiru@gmail.com>
2024-07-25 18:53:09 +0300

    deprecate beyondcompare script

--- deprecated/scripts.d/10-beyondcompare.sh
+++ deprecated/scripts.d/10-beyondcompare.sh
@@ -2,7 +2,7 @@
 
 set -u -e -o pipefail
 
-trap 'echo Script failed at line $LINENO with retcode $?' ERR TERM
+trap 'echo Script $BASH_SOURCE failed at line $LINENO with retcode $?' ERR TERM
 
 ## Install deps
 INSTALLER='sudo pacman -Sy --noconfirm --needed'
@@ -14,9 +14,9 @@ BCOMPARE_PREFIX=/usr/local
 DOWNLOAD_DEST=/tmp/bcompare
 [[ -d "$DOWNLOAD_DEST" ]] || mkdir -p "$DOWNLOAD_DEST"
 
-DOWNLOAD_URL=$(curl -sSL https://www.scootersoftware.com/beta/download | grep -oP '[^"]+\.tar\.gz(?=")')
+DOWNLOAD_URL=$(curl -sSL https://www.scootersoftware.com/kb/linux_install | grep -oP '[^"]+5.+\.tar\.gz(?=")')
 curl -L "https://www.scootersoftware.com$DOWNLOAD_URL" | tar -xz -C "$DOWNLOAD_DEST" --strip-components=1
 
 (cd "$DOWNLOAD_DEST" && sudo ./install.sh --prefix="$BCOMPARE_PREFIX")
 
-[[ -d "$DOWNLOAD_DEST" ]] && rm -fr "$DOWNLOAD_DEST"
+#[[ -d "$DOWNLOAD_DEST" ]] && rm -fr "$DOWNLOAD_DEST"
