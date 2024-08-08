bb8e7e23f9282ef6086753736e8256e1c499979a
Cristian Chiru <cristi.chiru@gmail.com>
2024-08-08 12:25:03 +0300

    add qt5/qt6 theme configuration via kvantum

--- scripts.d/07-desktop.sh
+++ scripts.d/07-desktop.sh
@@ -6,15 +6,21 @@ trap 'echo Script $BASH_SOURCE failed at line $LINENO with retcode $?' ERR TERM
 
 ## Fixes rendering issues with WebKitGTK based apps like Whatsapp, Minigalaxy, Journal Viewer
 grep -qP '^WEBKIT_DISABLE_DMABUF_RENDERER' /etc/environment || \
-  sudo sed -i -E 's,^[#\s]*(WEBKIT_DISABLE_DMABUF_RENDERER)=1,\1=1,' /etc/environment
+  sudo sed -i -E 's,^[#\s]*(WEBKIT_DISABLE_DMABUF_RENDERER).+,\1=1,' /etc/environment
+
+grep -qP '^QT_AUTO_SCREEN_SCALE_FACTOR' /etc/environment || \
+  sudo sed -i -E 's,^[#\s]*(QT_AUTO_SCREEN_SCALE_FACTOR).+,\1=1,' /etc/environment
+
+grep -qP '^QT_QPA_PLATFORMTHEME' /etc/environment || \
+  sudo sed -i -E 's,^[#\s]*(QT_QPA_PLATFORMTHEME).+,\1=qt6ct,' /etc/environment
 
 INSTALLER='sudo pacman -Sy --noconfirm --needed'
 $INSTALLER budgie network-manager-applet papirus-icon-theme budgie-desktop-view arc-gtk-theme \
   gnome-themes-extra gnome-console kitty gnome-calculator gnome-system-monitor gnome-keyring gnome-packagekit gnome-logs \
-  gvfs-smb appmenu-gtk-module dconf-editor font-manager xorg-xhost \
+  gvfs-smb appmenu-gtk-module dconf-editor font-manager xorg-xhost qt5ct qt6ct kvantum \
   eog-plugins libheif evince strawberry vlc obs-studio xcolor \
   meld nemo-preview nemo-share nemo-theme-glacier \
-  gedit baobab doublecmd-gtk2 libunrar freerdp remmina transmission-remote-gtk grsync gparted gsmartcontrol xarchiver \
+  gedit baobab doublecmd-qt6 libunrar freerdp remmina transmission-remote-gtk grsync gparted gsmartcontrol xarchiver \
   keepassxc x11-ssh-askpass \
   openrgb \
   thunderbird \
