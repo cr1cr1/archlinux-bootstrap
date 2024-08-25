709a08bf6689a80719b0925c843ceccdc606eeb6
Cristian Chiru <cristi.chiru@gmail.com>
2024-08-25 09:07:34 +0300

    add nvidia-settings

--- scripts.d/01-system-apps.sh
+++ scripts.d/01-system-apps.sh
@@ -12,7 +12,7 @@ which lshw &>/dev/null || $INSTALLER lshw
 
 ## NVIDIA Drivers
 if sudo lshw -C video -json | jq -r '.[].vendor=="NVIDIA Corporation"' | grep -q 'true'; then
-  paru -Sy --noconfirm --needed nvidia nvidia-patch opencl-nvidia lib32-nvidia-utils lib32-opencl-nvidia
+  paru -Sy --noconfirm --needed nvidia nvidia-settings nvidia-patch opencl-nvidia lib32-nvidia-utils lib32-opencl-nvidia
 fi
 
 ## System apps
