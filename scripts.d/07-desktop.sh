6f89fb70ffbb4280df5b923127f22359c0d9bb8e
Cristian Chiru <cristi.chiru@gmail.com>
2024-08-27 16:23:00 +0300

    add rnote

--- scripts.d/07-desktop.sh
+++ scripts.d/07-desktop.sh
@@ -18,7 +18,7 @@ INSTALLER='sudo pacman -Sy --noconfirm --needed'
 $INSTALLER budgie network-manager-applet papirus-icon-theme budgie-desktop-view arc-gtk-theme \
   gnome-themes-extra gnome-console kitty gnome-calculator gnome-system-monitor gnome-keyring gnome-logs \
   gvfs-smb appmenu-gtk-module dconf-editor font-manager xorg-xhost qt5ct qt6ct kvantum \
-  eog-plugins libheif evince strawberry vlc obs-studio xcolor \
+  eog-plugins libheif evince strawberry vlc obs-studio xcolor rnote \
   meld nemo-preview nemo-share nemo-theme-glacier nemo-fileroller \
   gedit baobab doublecmd-qt6 libunrar freerdp remmina transgui-qt grsync gparted gsmartcontrol xarchiver \
   keepassxc x11-ssh-askpass \
