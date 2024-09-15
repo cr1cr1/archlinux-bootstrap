4cd0d6653490353a3b25a07bb933b6c6010ded5b
Cristian Chiru <cristi.chiru@gmail.com>
2024-09-15 17:50:43 +0300

    yazi: add user and owner in status

--- scripts.d/.config/yazi/init.lua
+++ scripts.d/.config/yazi/init.lua
@@ -8,3 +8,26 @@ THEME.git_modified = ui.Style():fg("blue")
 THEME.git_deleted = ui.Style():fg("red"):bold()
 THEME.git_modified_sign = "M"
 THEME.git_deleted_sign = "D"
+
+-- https://yazi-rs.github.io/docs/tips#username-hostname-in-header
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ui.Line {}
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
+
+-- https://yazi-rs.github.io/docs/tips#user-group-in-status
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line {}
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	}
+end, 500, Status.RIGHT)
